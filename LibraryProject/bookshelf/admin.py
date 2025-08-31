1085782fbef32864ba902d39b0d5a0cf6c117fe4
natnaelian <natnaelian@gmail.com>
2025-09-01 00:19:11 +0300

    Fsd

--- LibraryProject/bookshelf/admin.py
+++ LibraryProject/bookshelf/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+# Register Book model with custom admin settings
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    # Fields to display in the list view
+    list_display = ("title", "author", "publication_year")
+
+    # Add filters to the right sidebar
+    list_filter = ("publication_year", "author")
+
+    # Enable search by title and author
+    search_fields = ("title", "author")
