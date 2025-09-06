b11de351f35697bc402c4fac40bdd4dfdcd4c23e
natnaelian <natnaelian@gmail.com>
2025-09-06 12:47:27 +0300

    fsdg

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import list_books, LibraryDetailView
+
+urlpatterns = [
+    # Function-based view
+    path("books/", list_books, name="list_books"),
+
+    # Class-based view
+    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
+]
