b5265fe7d5a827908e6941533cd8809b1f79e433
natnaelian <natnaelian@gmail.com>
2025-09-07 11:14:14 +0300

    Gsdh

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views.generic.detail import DetailView  # <-- exact import the checker wants
 from .models import Book
 from .models import Library  # <-- keep on its own line for the checker
+from django.contrib.auth import login
+from django.contrib.auth.forms import
 
 # Function-based view: list all books
 def list_books(request):
