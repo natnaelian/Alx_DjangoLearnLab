84783b4bfe76a5c14fcb8d8243690e303b920012
natnaelian <natnaelian@gmail.com>
2025-09-07 11:10:41 +0300

    gdssdg

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,15 +1,10 @@
 from django.urls import path
-from . import views
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
     # Function-based view
-    path("books/", views.list_books, name="list_books"),
+    path("books/", list_books, name="list_books"),
 
     # Class-based view
-    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
-
-    # Authentication views
-    path("register/", views.register_view, name="register"),
-    path("login/", views.login_view, name="login"),
-    path("logout/", views.logout_view, name="logout"),
+    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
 ]
