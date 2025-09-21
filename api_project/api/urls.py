a9e367fbee83ab198c9a5d31f046d11d4d7279de
natnaelian <natnaelian@gmail.com>
2025-09-21 19:36:33 +0300

    FDSgfs

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,7 @@
 # api/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 from .views import BookList, BookViewSet
 
 # Create router and register BookViewSet
@@ -10,6 +11,9 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 urlpatterns = [
     # Original ListAPIView (only GET all)
     path('books/', BookList.as_view(), name='book-list'),
+
+    # Token retrieval endpoint
+    path('auth-token/', obtain_auth_token, name='api_token_auth'),  
 
     # All CRUD operations via ViewSet
     path('', include(router.urls)),
